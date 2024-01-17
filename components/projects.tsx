a0e4828389da9f526c3dd52d3d6e34d3b02d5941
Mandip Giri <mandipgiri@SPMBP148.local>
2024-01-17 22:46:18 +0545

    inview into hook

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,22 +1,14 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
 
 import Project from './project'
-import { useInView } from 'react-intersection-observer'
-import { useActiveSectionContext } from '@/context/active-context'
+import { useSectionInView } from '@/lib/hooks'
 
 export default function Projects() {
-  const { ref, inView } = useInView({ threshold: 0.5 })
-  const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
-
-  useEffect(() => {
-    if (inView && Date.now() - timeOfLastClick > 1000) {
-      setActiveSection('Projects')
-    }
-  }, [inView, setActiveSection, timeOfLastClick])
+  const { ref } = useSectionInView('Projects')
 
   return (
     <section ref={ref} id="projects" className="mb-28 scroll-mt-28">
