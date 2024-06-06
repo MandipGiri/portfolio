bed9782989f73363a32ad305cd83825f2704f052
Mandip Giri <mandip.giri@cloudfactory.com>
2024-06-06 09:33:40 +0545

    updated config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
