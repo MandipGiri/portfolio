189043075bd3e554fe88b477376b52737b0b3868
Mandip Giri <mandip.giri@cloudfactory.com>
2024-06-12 10:00:37 +0545

    hidden sections

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,25 +1,25 @@
-'use client'
+// 'use client'
 
-import React from 'react'
-import SectionHeading from './section-heading'
-import { projectsData } from '@/lib/data'
+// import React from 'react'
+// import SectionHeading from './section-heading'
+// import { projectsData } from '@/lib/data'
 
-import Project from './project'
-import { useSectionInView } from '@/lib/hooks'
+// import Project from './project'
+// import { useSectionInView } from '@/lib/hooks'
 
-export default function Projects() {
-  const { ref } = useSectionInView('Projects')
+// export default function Projects() {
+//   const { ref } = useSectionInView('Projects')
 
-  return (
-    <section ref={ref} id="projects" className="mb-28 scroll-mt-28">
-      <SectionHeading>My Projects</SectionHeading>
-      <div>
-        {projectsData.map((project, idx) => (
-          <React.Fragment key={project.title}>
-            <Project {...project} />
-          </React.Fragment>
-        ))}
-      </div>
-    </section>
-  )
-}
+//   return (
+//     <section ref={ref} id="projects" className="mb-28 scroll-mt-28">
+//       <SectionHeading>My Projects</SectionHeading>
+//       <div>
+//         {projectsData.map((project, idx) => (
+//           <React.Fragment key={project.title}>
+//             <Project {...project} />
+//           </React.Fragment>
+//         ))}
+//       </div>
+//     </section>
+//   )
+// }
